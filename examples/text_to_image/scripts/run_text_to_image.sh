602ce404d47a1729e40213795ceb49f858f32f69
jiaqiw <wangjiaqi50@huawei.com>
2023-10-11 11:05:31 +0800

    text_to_image新增案例一个

---
+++ examples/text_to_image/scripts/run_text_to_image.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+cur_path=`pwd`
+cur_path_last_dirname=${cur_path##*/}
+if [ x"${cur_path_last_dirname}" == x"scripts" ];then
+    scripts_path_dir=${cur_path}
+    cd ..
+    cur_path=`pwd`
+else
+    scripts_path_dir=${cur_path}/scripts
+fi
+
+#创建输出目录，不需要修改
+if [ -d ${scripts_path_dir}/output ];then
+    rm -rf ${scripts_path_dir}/output
+    mkdir -p ${scripts_path_dir}/output
+else
+    mkdir -p ${scripts_path_dir}/output
+fi
+
+# 启动训练脚本
+start_time=$(date +%s)
+nohup python3 -m torch.distributed.run --nproc_per_node 8 train_text_to_image.py \
+  --pretrained_model_name_or_path CompVis/stable-diffusion-v1-4 \
+  --dataset_name lambdalabs/pokemon-blip-captions \
+  --use_ema \
+  --resolution=512 \
+  --center_crop \
+  --random_flip \
+  --train_batch_size=1 \
+  --gradient_accumulation_steps=4 \
+  --gradient_checkpointing \
+  --max_train_steps=50 \
+  --learning_rate=1e-05 \
+  --max_grad_norm=1 \
+  --lr_scheduler="constant" \
+  --lr_warmup_steps=0 \
+  --output_dir ./output > ${scripts_path_dir}/output/run_text_to_image.log 2>&1 &
+wait
+end_time=$(date +%s)
+e2e_time=$(( $end_time - $start_time ))
+
+# 打印端到端训练时间
+echo "E2E Training Duration sec : $e2e_time"
