69a873d12c37604dfabe4f9a27a0892d86db5a82
jiaqiw <wangjiaqi50@huawei.com>
2023-10-11 16:31:25 +0800

    text_to_image新增案例一个

---
+++ examples/text_to_image/scripts/run_text_to_image_sdxl.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+cur_path=`pwd`
+cur_path_last_dirname=${cur_path##*/}
+if [ x"${cur_path_last_dirname}" == x"scripts" ];then
+    scripts_path_dir=${cur_path}
+    cd ..
+    cur_path=`pwd`
+else
+    scripts_path_dir=${cur_path}/scripts
+fi
+
+#创建输出目录，不需要修改
+if [ -d ${scripts_path_dir}/output ];then
+    rm -rf ${scripts_path_dir}/output
+    mkdir -p ${scripts_path_dir}/output
+else
+    mkdir -p ${scripts_path_dir}/output
+fi
+
+# 启动训练脚本
+start_time=$(date +%s)
+nohup python3 -m torch.distributed.run --nproc_per_node 8 train_text_to_image_sdxl.py \
+  --pretrained_model_name_or_path stabilityai/stable-diffusion-xl-base-1.0 \
+  --pretrained_vae_model_name_or_path madebyollin/sdxl-vae-fp16-fix\
+  --dataset_name lambdalabs/pokemon-blip-captions \
+  --enable_xformers_memory_efficient_attention \
+  --resolution=512 \
+  --center_crop \
+  --random_flip \
+  --proportion_empty_prompts=0.2 \
+  --train_batch_size=1 \
+  --gradient_accumulation_steps=4 \
+  --gradient_checkpointing \
+  --max_train_steps=10000 \
+  --use_8bit_adam \
+  --learning_rate=1e-06 \
+  --lr_scheduler="constant" \
+  --lr_warmup_steps=0 \
+  --mixed_precision="fp16" \
+  --report_to="wandb" \
+  --validation_prompt="a cute Sundar Pichai creature" \
+  --validation_epochs 5 \
+  --checkpointing_steps=5000 \
+  --output_dir ./output > ${scripts_path_dir}/output/run_text_to_image_sdxl.log 2>&1 &
+wait
+end_time=$(date +%s)
+e2e_time=$(( $end_time - $start_time ))
+
+# 打印端到端训练时间
+echo "E2E Training Duration sec : $e2e_time"
